9d47cfcf9dd66329b77a73a391cebaa092c1aeef
RamizIb <RamizIb@yandex.ru>
2017-01-08 12:49:36 +0300

    Build test

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -13,7 +15,11 @@ gulp.task("style", function() {
     .pipe(less())
     .pipe(postcss([
        autoprefixer({browsers: [
-         "last 2 versions"
+         "last 2 versions",
+         "last 2 Chrome versions",
+         "last 2 Firefox versions",
+         "last 2 Opera versions",
+         "last 2 Edge versions"
         ]})
     ]))
     .pipe(gulp.dest("css"))
@@ -31,4 +37,32 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+
+
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/**",
+    "img/**",
+    "js/**",
+    "*.html"
+    ], {
+      base: "."
+    })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    fn
+  );
 });
