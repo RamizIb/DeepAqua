69af819abc896f256870f52b6bd64b322bf468d2
RamizIb <RamizIb@yandex.ru>
2017-04-11 18:04:16 +0300

    Php form handler, payment choice product page

---
+++ send-product-form.php
@@ -0,0 +1,18 @@
+<meta http-equiv='refresh' content='3; url=https://aquel-shop.ru/'>
+<meta charset="UTF-8" />
+
+<?php
+if (isset($_POST['product-form-name'])) {$name = $_POST['product-form-name']; if ($name == '') {unset($name);}}
+if (isset($_POST['product-form-phone'])) {$phone = $_POST['product-form-phone']; if ($phone == '') {unset($phone);}}
+
+if (isset($name) && isset($phone)){
+
+  $address = "aquel-shop@yandex.ru";
+  $mes = "(Форма на странице товара - консультация.\nИмя: $name \nТелефон: $phone";
+  $send = mail ($address,"Content-type:text/plain; charset = UTF-8\r\n");
+  if ($send == 'true')
+    {echo "Сообщение отправлено успешно, через 3 секунды Вы вернетесь на сайт";}
+  else {echo "Ошибка, сообщение не отправлено!";}
+
+}
+?>
